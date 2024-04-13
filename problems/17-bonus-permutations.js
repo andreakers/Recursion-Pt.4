37f92ad241ed47711da27fe32791b88fa104afb7
Andre <akers.andre@yahoo.com>
2024-04-13 14:55:36 -0700

    17 Done

--- problems/17-bonus-permutations.js
+++ problems/17-bonus-permutations.js
@@ -13,10 +13,31 @@ permutations([1, 2, 3]) // [[1, 2, 3], [1, 3, 2],
                         // [2, 1, 3], [2, 3, 1],
                         // [3, 1, 2], [3, 2, 1]]
 ***********************************************************************/
-
+debugger
 const permutations = (array) => {
   // Your code here 
+  let result = [];
+
+  if (array.length === 0) return [];
+  if (array.length === 1) return [array];
+
+  for (let i = 0; i < array.length; i++) {
+    const currentNum = array[i];
+    const remainingNums = array.slice(0, i).concat(array.slice(i + 1));
+    const remainingNumsPermuted = permutations(remainingNums);
+
+    for (let j = 0; j < remainingNumsPermuted.length; j++) {
+      const permutedArray = [currentNum].concat(remainingNumsPermuted[j]);
+      result.push(permutedArray)
+    }
+  }
+  return result
 };
+
+console.log(permutations([1, 2])) // [[1, 2], [2, 1]]
+console.log(permutations([1, 2, 3])) // [[1, 2, 3], [1, 3, 2],
+                        // [2, 1, 3], [2, 3, 1],
+                        // [3, 1, 2], [3, 2, 1]]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
