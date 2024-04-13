64425f23845de3b90c774170794c8b0077c54ecd
Andre <akers.andre@yahoo.com>
2024-04-13 09:54:30 -0700

    13 Done

--- problems/13-sort.js
+++ problems/13-sort.js
@@ -20,10 +20,23 @@ sort([4,1,6,3,1,7]); // [1, 1, 3, 4, 6, 7]
 sort([0, 1, -3]); // [-3, 0, 1]
 sort([]); // []
 ***********************************************************************/
-
+debugger
 function sort(nums, sorted = []) {
     // Your code here 
+    if (nums.length === 0) {
+        return sorted
+    }
+
+    let minNum = Math.min(...nums)
+    sorted.push(minNum)
+    nums.splice(nums.indexOf(minNum), 1)
+    console.log(nums)
+    return sort(nums, sorted)
 }
+
+console.log(sort([4,1,6,3,1,7])); // [1, 1, 3, 4, 6, 7]
+console.log(sort([0, 1, -3])); // [-3, 0, 1]
+console.log(sort([])); // []
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = sort;
